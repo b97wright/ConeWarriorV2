64c1b12276fe745451cbe925f0a54d73c2f1e6cf
b97wright <110114784+b97wright@users.noreply.github.com>
2024-09-02 11:10:12 -0400

    11. Native Gameplay Tags

---
+++ Source/ConeWarriorV2/Private/ConeWarriorGameplayTags.cpp
@@ -0,0 +1,12 @@
+// Island Cone Games
+
+
+#include "ConeWarriorGameplayTags.h"
+
+
+namespace ConeWarriorGameplayTags
+{
+	/** Input Tags **/
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move")
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look")
+}
---
+++ Source/ConeWarriorV2/Public/ConeWarriorGameplayTags.h
@@ -0,0 +1,12 @@
+// Island Cone Games
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace ConeWarriorGameplayTags
+{
+	/** Input Tags **/
+	CONEWARRIORV2_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move)
+	CONEWARRIORV2_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look)
+}
