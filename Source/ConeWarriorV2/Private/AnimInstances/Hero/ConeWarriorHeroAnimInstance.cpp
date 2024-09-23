04a383c7803908a9bbdd49ff6fbb343bf573d9de
b97wright <110114784+b97wright@users.noreply.github.com>
2024-09-23 10:43:37 -0400

    17. Hero Anim Instance

--- Source/ConeWarriorV2/Private/AnimInstances/Hero/ConeWarriorHeroAnimInstance.cpp
+++ Source/ConeWarriorV2/Private/AnimInstances/Hero/ConeWarriorHeroAnimInstance.cpp
@@ -2,4 +2,34 @@
 
 
 #include "AnimInstances/Hero/ConeWarriorHeroAnimInstance.h"
+#include "Characters/ConeWarriorHeroCharacter.h"
 
+void UConeWarriorHeroAnimInstance::NativeInitializeAnimation()
+{
+
+	Super::NativeInitializeAnimation();
+
+	if (OwningCharacter)
+	{
+		OwningConeWarriorHeroCharacter = Cast<AConeWarriorHeroCharacter>(OwningCharacter);
+	}
+
+}
+
+void UConeWarriorHeroAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+
+	if (bHasAcceleration)
+	{
+		IdleElpasedTime = 0.f;
+		bShouldEnterRelaxState = false;
+	}
+	else
+	{
+		IdleElpasedTime += DeltaSeconds;
+		bShouldEnterRelaxState = (IdleElpasedTime >= EnterRelaxStateThreshold);
+	}
+
+}
--- Source/ConeWarriorV2/Public/AnimInstances/Hero/ConeWarriorHeroAnimInstance.h
+++ Source/ConeWarriorV2/Public/AnimInstances/Hero/ConeWarriorHeroAnimInstance.h
@@ -6,6 +6,8 @@
 #include "AnimInstances/ConeWarriorCharacterAnimInstance.h"
 #include "ConeWarriorHeroAnimInstance.generated.h"
 
+class AConeWarriorHeroCharacter;
+
 /**
  * 
  */
@@ -13,5 +15,23 @@ UCLASS()
 class CONEWARRIORV2_API UConeWarriorHeroAnimInstance : public UConeWarriorCharacterAnimInstance
 {
 	GENERATED_BODY()
+
+public:
+
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds);
+
+protected:
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|References")
+	AConeWarriorHeroCharacter* OwningConeWarriorHeroCharacter;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	bool bShouldEnterRelaxState;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	float EnterRelaxStateThreshold = 5.f;
+
+	float IdleElpasedTime;
 	
 };
