8cec9facc62a504697ab4f0a71ee55b7ae14f3ab
b97wright <110114784+b97wright@users.noreply.github.com>
2024-09-26 15:02:31 -0400

    19.Ability Activation Policy

---
+++ Source/ConeWarriorV2/Public/AbilitySystem/Abilities/ConeWarriorGameplayAbility.h
@@ -0,0 +1,35 @@
+// Island Cone Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "ConeWarriorGameplayAbility.generated.h"
+
+
+UENUM(BlueprintType)
+enum class EConeWarriorAbilityActivationPolicy : uint8
+{
+	OnTriggered,
+	OnGiven
+};
+
+/**
+ * 
+ */
+UCLASS()
+class CONEWARRIORV2_API UConeWarriorGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+protected: 
+
+	//~ Begin UGameplayAbility Interface.
+	virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+	//~ Begin UGameplayAbility Interface.
+
+	UPROPERTY(EditDefaultsOnly, Category = "ConeWarriorAbility")
+	EConeWarriorAbilityActivationPolicy AbilityActivationPolicy = EConeWarriorAbilityActivationPolicy::OnTriggered;
+	
+};
