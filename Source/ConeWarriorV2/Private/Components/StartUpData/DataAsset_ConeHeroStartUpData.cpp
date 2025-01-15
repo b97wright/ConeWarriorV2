400e0794873405e8c69a39f26b23a25e442ee9d7
b97wright <110114784+b97wright@users.noreply.github.com>
2025-01-15 11:09:03 -0500

    32 Hero Ability Set
    Will need to check some things though

--- Source/ConeWarriorV2/Private/Components/StartUpData/DataAsset_ConeHeroStartUpData.cpp
+++ Source/ConeWarriorV2/Private/Components/StartUpData/DataAsset_ConeHeroStartUpData.cpp
@@ -2,4 +2,28 @@
 
 
 #include "Components/StartUpData/DataAsset_ConeHeroStartUpData.h"
+#include "AbilitySystem/Abilities/ConeWarriorGameplayAbility.h"
+#include "AbilitySystem/ConeWarAbilitySystemComponent.h"
 
+bool FConeWarriorHeroAbilitySet::IsValid() const
+{
+    return InputTag.IsValid() && AbilityToGrant;
+}
+
+void UDataAsset_ConeHeroStartUpData::GiveToAbilitySystemComponent(UConeWarAbilitySystemComponent* InASCToGive, int32 ApplyLevel)
+{
+    Super::GiveToAbilitySystemComponent(InASCToGive, ApplyLevel);
+
+    for (const FConeWarriorHeroAbilitySet& AbilitySet : HeroStartUpAbilitySets)
+    {
+        if (!AbilitySet.IsValid()) continue;
+
+        FGameplayAbilitySpec AbilitySpec(AbilitySet.AbilityToGrant);
+        AbilitySpec.SourceObject = InASCToGive->GetAvatarActor();
+        AbilitySpec.Level = ApplyLevel;
+        AbilitySpec.DynamicAbilityTags.AddTag(AbilitySet.InputTag);
+
+        InASCToGive->GiveAbility(AbilitySpec);
+    }
+
+}
--- Source/ConeWarriorV2/Public/Components/StartUpData/DataAsset_ConeHeroStartUpData.h
+++ Source/ConeWarriorV2/Public/Components/StartUpData/DataAsset_ConeHeroStartUpData.h
@@ -4,7 +4,23 @@
 
 #include "CoreMinimal.h"
 #include "Components/StartUpData/DataAsset_StartUpDataBase.h"
+#include "GameplayTagContainer.h"
 #include "DataAsset_ConeHeroStartUpData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FConeWarriorHeroAbilitySet
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (Categories = "InputTag"))
+	FGameplayTag InputTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UConeWarriorGameplayAbility> AbilityToGrant;
+
+	bool IsValid() const;
+
+};
 
 /**
  * 
@@ -13,5 +29,14 @@ UCLASS()
 class CONEWARRIORV2_API UDataAsset_ConeHeroStartUpData : public UDataAsset_StartUpDataBase
 {
 	GENERATED_BODY()
+
+public:
+
+	virtual void GiveToAbilitySystemComponent(UConeWarAbilitySystemComponent* InASCToGive, int32 ApplyLevel = 1) override;
+
+private:
+
+	UPROPERTY(EditDefaultsOnly, Category = "StartUpData", meta = (TitleProperty = "InputTag"))
+	TArray<FConeWarriorHeroAbilitySet> HeroStartUpAbilitySets;
 	
 };
